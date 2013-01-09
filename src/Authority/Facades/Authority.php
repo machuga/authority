ac7c875dc6eed01fee91abbfbce0acd1062a9c31
wishoundry <bngreer@gmail.com>
2013-01-08 17:33:10 -0800

    Facade

---
+++ src/Authority/Facades/Authority.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * __      ___    _    ___                 _          
+ * \ \    / (_)__| |_ | __|__ _  _ _ _  __| |_ _ _  _ 
+ *  \ \/\/ /| (_-< ' \| _/ _ \ || | ' \/ _` | '_| || |
+ *   \_/\_/ |_/__/_||_|_|\___/\_,_|_||_\__,_|_|  \_, |
+ *                                               |__/
+ *                                                                               
+ * Created by : bngreer
+ * Date: 1/8/13
+ * Time: 5:32 PM       
+ * Copyright 2013 The WishFoundry / Ben Greer. All Rights Reserved.
+ * 
+ * 
+ */ 
+class Authority {
+
+}
